6c3c4bab7c5b6212c09fc002d123b3f099bf18c5
Nolexx07 <sunnysathvik507@gmail.com>
2026-07-17 00:29:34 +0530

    first commit

---
+++ js/google-map.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    var mapElement = document.getElementById('map');
+    if (!mapElement) return;
+
+    // Center coordinates for the restaurant (New York)
+    var lat = 40.69847;
+    var lng = -73.95144;
+
+    // Initialize Leaflet map
+    var map = L.map('map', {
+        scrollWheelZoom: false
+    }).setView([lat, lng], 13);
+
+    // Use premium CartoDB Dark Matter tiles to match the premium dark theme
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
+    }).addTo(map);
+
+    // Custom pizza marker icon using flaticon stylesheet already loaded in page
+    var pizzaIcon = L.divIcon({
+        className: 'custom-pizza-marker',
+        html: '<span class="flaticon-pizza-1" style="font-size: 32px; color: #fac564; display: block; filter: drop-shadow(0 2px 8px rgba(0,0,0,0.5));"></span>',
+        iconSize: [32, 32],
+        iconAnchor: [16, 16],
+        popupAnchor: [0, -16]
+    });
+
+    // Add marker to map
+    L.marker([lat, lng], { icon: pizzaIcon }).addTo(map)
+        .bindPopup('<b style="color: #1e1812; font-family:\'Plus Jakarta Sans\', sans-serif;">Pizza Restaurant</b><br><span style="color: #666;">198 West 21th Street, New York</span>')
+        .openPopup();
+});
